6222134fec267e185c40d33d8b960219d88a29e0
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-02-25 12:39:43 +0500

    new update

--- CleanArchitecture/Program.cs
+++ CleanArchitecture/Program.cs
@@ -1,3 +1,5 @@
+using UserAPI.Infostructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddContexts(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
--- UserAPI.Infostructure/Persistence/ApplicationDBCore.cs
+++ UserAPI.Infostructure/Persistence/ApplicationDBCore.cs
@@ -3,16 +3,15 @@ using UserAPI.Domen.Model.UserModels.Models;
 
 namespace UserAPI.Infostructure.Persistence
 {
-    public class ApplicationDBCore : DbContext
+  
+    public class ApplicationDbContext : DbContext
     {
-        public class ApplicationDbContext : DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
-            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-                : base(options)
-            {
 
-            }
-            public DbSet<UserProfile> UserProfiles { get; set; }
         }
+        public DbSet<UserProfile> UserProfiles { get; set; }
     }
 }
+
